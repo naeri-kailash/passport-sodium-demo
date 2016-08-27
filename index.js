08a93e307b8a04b271534443e7e8f794627c6661
Rich Churcher <rich.churcher@gmail.com>
2016-08-28 10:42:23 +1200

    Move routes to router

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const bodyParser = require('body-parser')
-const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn
 const express = require('express')
 const expressSession = require('express-session')
 const flash = require('connect-flash')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
-const sodium = require('sodium').api
 
 const localSodium = require('./lib/local-sodium')
-const development = require('./knexfile').development
-const knex = require('knex')(development)
+const indexRoutes = require('./routes')
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -24,75 +21,11 @@ app.use(expressSession({
 app.use(flash())
 app.use(passport.initialize())
 app.use(passport.session())
+app.use('/', indexRoutes)
 
 passport.use(new LocalStrategy(localSodium.strategy))
 passport.serializeUser(localSodium.serialize)
 passport.deserializeUser(localSodium.deserialize)
-
-app.get('/login', (req, res) => {
-  res.send(`<p>${req.flash('error')}</p><form action="login" method="POST"><input name="username"><input name="password"><input type="submit" value="Log in"></form>`)
-})
-
-app.post('/login',
-  passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-  }))
-
-app.get('/logout', (req, res) => {
-  req.logout()
-  res.redirect('/login')
-})
-
-app.get('/',
-  ensureLoggedIn(),
-  (req, res) => {
-    res.send('Yup.')
-  })
-
-app.get('/register', (req, res) => {
-  res.send(`<p>${req.flash('error')}</p><form action="register" method="POST"><input name="username"><input name="password"><input type="submit" value="Register"></form>`)
-})
-
-app.post('/register', (req, res) => {
-  // Check username doesn't already exist
-  knex('users')
-    .count('id as n')
-    .where('username', req.body.username)
-    .then(count => {
-      if (count[0].n > 0) {
-        req.flash('error', 'User already exists, sorry.')
-        return res.redirect('/register')
-      }
-      if (!req.body.password || !req.body.username) {
-        req.flash('error', "You'll need a username and password.")
-        return res.redirect('/register')
-      }
-
-      // Don't store the password in plaintext: hash it with libsodium
-      const pbuf = new Buffer(req.body.password, 'utf8')
-      const hash = sodium.crypto_pwhash_str(
-        pbuf,
-        sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE,
-        sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE
-      )
-
-      // Add the user to the database
-      knex('users')
-        .insert({
-          username: req.body.username,
-          password: hash.toString()
-        })
-        .then(() => {
-          res.redirect('/login')
-        })
-    })
-    .catch(() => {
-      req.flash('error', "Couldn't add user.")
-      res.redirect('/register')
-    })
-})
 
 app.listen(3000, () => {
   console.log('Listening on 3000')
---
+++ routes/index.js
@@ -0,0 +1,75 @@
+const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn
+const express = require('express')
+const passport = require('passport')
+const sodium = require('sodium').api
+
+const development = require('../knexfile').development
+const knex = require('knex')(development)
+
+const router = express.Router()
+module.exports = router
+
+router.get('/login', (req, res) => {
+  res.send(`<p>${req.flash('error')}</p><form action="login" method="POST"><input name="username"><input name="password"><input type="submit" value="Log in"></form>`)
+})
+
+router.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  }))
+
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/login')
+})
+
+router.get('/',
+  ensureLoggedIn(),
+  (req, res) => {
+    res.send('Yup.')
+  })
+
+router.get('/register', (req, res) => {
+  res.send(`<p>${req.flash('error')}</p><form action="register" method="POST"><input name="username"><input name="password"><input type="submit" value="Register"></form>`)
+})
+
+router.post('/register', (req, res) => {
+  // Check username doesn't already exist
+  knex('users')
+    .count('id as n')
+    .where('username', req.body.username)
+    .then(count => {
+      if (count[0].n > 0) {
+        req.flash('error', 'User already exists, sorry.')
+        return res.redirect('/register')
+      }
+      if (!req.body.password || !req.body.username) {
+        req.flash('error', "You'll need a username and password.")
+        return res.redirect('/register')
+      }
+
+      // Don't store the password in plaintext: hash it with libsodium
+      const pbuf = new Buffer(req.body.password, 'utf8')
+      const hash = sodium.crypto_pwhash_str(
+        pbuf,
+        sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE,
+        sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE
+      )
+
+      // Add the user to the database
+      knex('users')
+        .insert({
+          username: req.body.username,
+          password: hash.toString()
+        })
+        .then(() => {
+          res.redirect('/login')
+        })
+    })
+    .catch(() => {
+      req.flash('error', "Couldn't add user.")
+      res.redirect('/register')
+    })
+})
