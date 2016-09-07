90b01c5bd03210dfa610c24103a3b4e1ef7199db
Rich Churcher <rich.churcher@gmail.com>
2016-09-08 09:47:09 +1200

    Passport local for one route

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,7 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const authenticate = require('express-jwt')
 const jwt = require('jsonwebtoken')
-
-const crypto = require('../lib/crypto')
-const users = require('../lib/users')
+const passport = require('passport')
 
 const router = express.Router()
 module.exports = router
@@ -16,33 +14,31 @@ router.use(bodyParser.json())
 //   "password": "bar"
 // }
 // This should obviously be sent via HTTPS.
-router.post('/authenticate', (req, res) => {
-  users.getByName(req.body.username)
-    .then(userList => {
-      if (userList.length === 0) {
-        return res.json({ message: 'Authentication failed. User not found.' })
-      }
-
-      const user = userList[0]
-      if (!crypto.verifyUser(user, req.body.password)) {
-        return res.json({ message: 'Authentication failed. Wrong password.' })
-      }
-
-      const token = jwt.sign({ id: user.id }, req.app.get('AUTH_SECRET'), {
-        expiresIn: 60 * 60 * 24
-      })
-
-      res.json({
-        message: 'Authentication successful.',
-        token: token
-      })
-    })
-    .catch(err => {
-      res.status(500).json({
+router.post('/authenticate', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      return res.status(500).json({
         message: 'Authentication failed due to a server error.',
         error: err.message
       })
+    }
+
+    if (!req.user) {
+      return res.json({
+        message: 'Authentication failed.',
+        info: info
+      })
+    }
+
+    const token = jwt.sign({ id: user.id }, req.app.get('AUTH_SECRET'), {
+      expiresIn: 60 * 60 * 24
     })
+
+    res.json({
+      message: 'Authentication successful.',
+      token: token
+    })
+  })(req, res)
 })
 
 function handleAuthError (err, req, res, next) {
