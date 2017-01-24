921f472a5b4f6181a4ae16903da6a47c8a8f9ba5
Rich Churcher <rich.churcher@gmail.com>
2017-01-25 10:33:39 +1300

    Refactor

--- index.js
+++ index.js
@@ -1,41 +1,4 @@
-const express = require('express')
-const hbs = require('express-handlebars')
-const flash = require('connect-flash')
-const path = require('path')
-const passport = require('passport')
-const LocalStrategy = require('passport-local')
-
-const config = require('./knexfile')[process.env.NODE_ENV || 'development']
-const knex = require('knex')(config)
-const expressSession = require('express-session')
-const KnexSessionStore = require('connect-session-knex')(expressSession)
-const auth = require('./lib/auth')
-const users = require('./lib/users')
-
-const apiRoutes = require('./routes/api')
-const indexRoutes = require('./routes')
-
-const app = express()
-app.engine('hbs', hbs())
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'views'))
-
-app.use(expressSession({
-  resave: false,
-  saveUninitialized: false,
-  secret: process.env.SESSION_SECRET,
-  store: new KnexSessionStore({ knex: knex })
-}))
-
-app.use(flash())
-app.use(passport.initialize())
-app.use(passport.session())
-app.use('/', indexRoutes)
-app.use('/api/', apiRoutes)
-
-passport.use(new LocalStrategy(auth.verify))
-passport.serializeUser(users.serialize)
-passport.deserializeUser(users.deserialize)
+const app = require('./server')
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const flash = require('connect-flash')
+const hbs = require('express-handlebars')
+const LocalStrategy = require('passport-local')
+const passport = require('passport')
+const path = require('path')
+
+const auth = require('./lib/auth')
+const users = require('./lib/users')
+const session = require('./lib/session')
+
+const apiRoutes = require('./routes/api')
+const indexRoutes = require('./routes')
+
+const app = express()
+app.engine('hbs', hbs())
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(session)
+app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use('/', indexRoutes)
+app.use('/api/', apiRoutes)
+
+passport.use(new LocalStrategy(auth.verify))
+passport.serializeUser(users.serialize)
+passport.deserializeUser(users.deserialize)
+
+module.exports = app
+
