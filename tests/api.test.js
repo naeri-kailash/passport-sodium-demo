cddc943fcc8efa0fef3b6cab2bf33c9330a756c2
Rich Churcher <rich.churcher@gmail.com>
2017-01-26 12:33:11 +1300

    More API tests, remove knex

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,20 +1,12 @@
 const test = require('ava')
-const knex = require('knex')
 const request = require('supertest')
 
 const app = require('../server')
 
-const config = require('../knexfile').test
-
-test.beforeEach(t => {
+test.before(t => {
+  // Ignore the fact that the cert is unsigned
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-  t.context.db = knex(config)
-  return t.context.db
-    .migrate.latest()
-    .then(() => t.context.db.seed.run())
 })
-
-test.afterEach(t => t.context.db.destroy())
 
 test.cb('Redirects requests without SSL', t => {
   request(app.http)
@@ -34,6 +26,27 @@ test.cb('Authenticate complains about no credentials', t => {
     .end((err, res) => {
       t.ifError(err)
       t.is(res.body.info, 'Missing credentials')
+      t.end()
+    })
+})
+
+test.cb('/api/open responds without token', t => {
+  request(app.https)
+    .get('/api/open')
+    .end((err, res) => {
+      t.ifError(err)
+      t.deepEqual(res.body, { message: 'This route is public.' })
+      t.end()
+    })
+})
+
+test.cb("/api/closed 403's without token", t => {
+  request(app.https)
+    .get('/api/closed')
+    .expect(403)
+    .end((err, res) => {
+      t.ifError(err)
+      t.is(res.body.error, 'No authorization token was found')
       t.end()
     })
 })
