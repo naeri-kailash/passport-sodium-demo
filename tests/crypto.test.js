4a8116894556f32ef183f8614a9d4330481e25b7
Rich Churcher <rich.churcher@gmail.com>
2017-01-26 12:41:15 +1300

    Avoid users & db

--- tests/crypto.test.js
+++ tests/crypto.test.js
@@ -1,19 +1,7 @@
-const knex = require('knex')
 const sodium = require('sodium').api
 const test = require('ava')
 
-const config = require('../knexfile').test
 const crypto = require('../lib/crypto')
-const users = require('../lib/users')
-
-test.beforeEach(t => {
-  t.context.db = knex(config)
-  return t.context.db
-    .migrate.latest()
-    .then(() => t.context.db.seed.run())
-})
-
-test.afterEach(t => t.context.db.destroy())
 
 test('getHash returns verifiable hash in buffer', t => {
   const password = Buffer.from('password', 'utf8')
@@ -22,12 +10,19 @@ test('getHash returns verifiable hash in buffer', t => {
 })
 
 test('verifyUser verifies user with correct password', t => {
-  // Passwords same as usernames in test db!
-  return users.getByName('aardvark', t.context.db)
-    .then(([ user ]) => t.truthy(crypto.verifyUser(user, 'aardvark')))
+  const hash = sodium.crypto_pwhash_str(
+    Buffer.from('aardvark', 'utf8'),
+    sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE,
+    sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE
+  )
+  t.truthy(crypto.verifyUser({ hash }, 'aardvark'))
 })
 
 test('verifyUser does not veryfiy user with incorrect password', t => {
-  return users.getByName('capybara', t.context.db)
-    .then(([ user ]) => t.falsy(crypto.verifyUser(user, 'wrong password')))
+  const hash = sodium.crypto_pwhash_str(
+    Buffer.from('capybara', 'utf8'),
+    sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE,
+    sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE
+  )
+  t.falsy(crypto.verifyUser({ hash }, 'wrong password'))
 })
